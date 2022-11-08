de137f880d46f37e47a576df9496afdec74fd47d
Dilan Madhusanka <kadm0128@gmail.com>
2022-11-08 13:14:31 +0530

    NEW | Client side routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Classes } from "./components/Classes";
+import { Teachers } from "./components/Teachers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <div className="menu-bar">
+                    <ul style={{ listStyleType: "none" }}>
+                        <li>
+                            <Link to="/">
+                                <img
+                                    src={logo}
+                                    className="App-logo"
+                                    alt="logo"
+                                />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/classes">Classes</Link>
+                        </li>
+                        <li>
+                            <Link to="/teacher">Teacher</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/classes" element={<Classes />} />
+                        <Route path="/teacher" element={<Teachers />} />
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
