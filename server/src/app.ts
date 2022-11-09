81513ec5f0c6f0611faab20af27a0cd5439531fc
Dilan Madhusanka <kadm0128@gmail.com>
2022-11-09 09:44:41 +0530

    NEW | Client side code refactoring

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,8 @@ import cors from "cors";
 import express from "express";
 import errorHandler from "./middleware/errorHandler";
 import fourOhFour from "./middleware/fourOhFour";
-import root from "./routes/root";
+import teacher from "./routes/teacher";
+import subject from "./routes/subject";
 
 const app = express();
 
@@ -10,7 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/", root);
+app.use("/", teacher);
+app.use("/", subject);
 
 app.use(fourOhFour);
 app.use(errorHandler);
