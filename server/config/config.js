6d7ed31007435013ae7845a0fcdbea556cf99a7a
Dilan Madhusanka <kadm0128@gmail.com>
2022-11-09 12:45:43 +0530

    CHG | Database configurations change

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,5 @@
 module.exports = {
     development: {
-        // username: process.env['USERNAME'],
-        // password: process.env['PASSWORD'],
-        // database: process.env['DATABASE'],
-        // host: process.env['HOST'],
         username: 'root',
         password: '123456',
         database: 'education_business',
